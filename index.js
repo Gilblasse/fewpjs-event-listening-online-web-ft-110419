3f14d2e0d0d55873a950f5a9d4c7420bf8117079
Nethelbert <gilblasse@gmail.com>
2020-02-19 06:21:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const input = document.getElementById('main');
 function addingEventListener() {
   
   input.addEventListener('click', function(event) {
-    return alert('I was clicked!');
+    alert('I was clicked!');
   });
 }
 
